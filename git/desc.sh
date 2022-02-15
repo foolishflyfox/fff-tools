559f68c0834ae6f943ae85634f1665fcccd7e6e9
benfeihu <benfeihu@pinduoduo.com>
2022-02-15 14:29:47 +0800

    fix git/desc.sh

--- git/desc.sh
+++ git/desc.sh
@@ -2,24 +2,26 @@
 
 helpFun() {
     echo "显示当前分支描述: fff git/desc.sh"
-    echo "设置当前分支描述: fff git/desc.sh 描述"
+    echo "查询指定分支描述: fff git/desc.sh 分支(可模糊查询)"
+    echo "设置当前分支描述: fff git/desc.sh [-s|--set] 描述"
     echo "特定分支添加描述: fff git/desc.sh 分支名 描述"
-    echo "查询指定分支描述: fff git/desc.sh -b 分支(可模糊查询)"
-    echo "查询指定分支描述: fff git/desc.sh --branch 分支(可模糊查询)"
 }
 
 if [ $# = 0 ] ; then
     git br | grep $(git branch --show-current) | grep "*"
 elif [ $# = 1 ] ; then
-    if [ $1 = "-h" ] || [ $1 = "--help" ] || [ $1 = "-b" ] || [ $1 = "--branch" ]; then
+    if [ $1 = "-h" ] || [ $1 = "--help" ] || [ $1 = "-s" ] || [ $1 = "--set" ]; then
         helpFun
     else
-        git config branch.$(git branch --show-current).description $1
+        # 查询特定分支
+        git br | grep $1
     fi
 elif [ $# = 2 ] ; then
-    if [ $1 = "-b" ] || [ $1 = "--branch" ]; then
-        git br | grep $2
+    if [ $1 = "-s" ] || [ $1 = "--set" ]; then
+        # 设置当前分支描述
+        git config branch.$(git branch --show-current).description $2
     else
+        # 设置指定分支描述
         git config branch.$1.description $2
     fi
 else
