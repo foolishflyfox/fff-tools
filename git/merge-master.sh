990ebb36503a6f2f35464efd7af43d757d81fd62
benfeihu <benfeihu@pinduoduo.com>
2022-02-15 14:13:06 +0800

    update git/tools

--- git/merge-master.sh
+++ git/merge-master.sh
@@ -1,3 +1,12 @@
 #! /bin/zsh
 
-git fetch origin && git pull && git merge origin/master
+# git fetch origin && git pull && git merge origin/master
+
+# CURRENT_BRANCH=$(git branch --show-current)
+# git checkout master
+# git pull
+# git checkout $CURRENT_BRANCH
+# git merge master
+
+$FFF_DIR/git/merge.sh master
+
--- git/merge.sh
+++ git/merge.sh
@@ -1,7 +1,12 @@
 #! /bin/zsh
 
+CURRENT_BRANCH=$(git branch --show-current)
 if  [ -z "$1" ]; then
     echo "fff git/merge.sh 分支名"
 else
-    git fetch origin && git pull && git merge origin/$1
+    # git fetch origin && git pull && git merge origin/$1
+	git checkout $1
+	git pull
+	git checkout $CURRENT_BRANCH
+	git merge $1
 fi
