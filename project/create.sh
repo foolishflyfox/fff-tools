39f2865007ff658c1867bd9ff6f8064da301a125
foolishflyfox <fenghuabin1992@163.com>
2022-03-07 03:13:55 +0800

    update

--- project/create.sh
+++ project/create.sh
@@ -4,7 +4,8 @@ GRADLE_VERSION_4=1
 GRADLE_VERSION_5=2
 
 def createJavaGradleSpringbootProject() {
-    project_name="gradle$gradle_version-springboot"
+    source_dir="gradle$gradle_version-springboot"
+    project_name=$source_dir
     echo -n "please input project name (default is $project_name):"
     read tmp
     if [ $tmp ]; then
@@ -12,7 +13,7 @@ def createJavaGradleSpringbootProject() {
     fi
     echo "creating $project_name ..."
     echo "\tgradle version: $gradle_version"
-    cp -r $FFF_DIR/project/gradle4-springboot ./$project_name
+    cp -r $FFF_DIR/project/$source_dir ./$project_name
     echo "rootProject.name = '$project_name'" > $project_name/settings.gradle
     mkdir -p $project_name/src/{main/{java,resources},test/{java,resources}}
     echo "finished! enjoy it ^_^"
@@ -28,12 +29,12 @@ export build_tool=gradle
 echo "build tool: $build_tool"
 
 # 选择 gradle 的版本号
-if [ $build_tool="gradle" ]; then
-    echo -n "choose gradle version [4 - 4.10, 5 - 5.2.1]:"
+if [ $build_tool = "gradle" ]; then
+    echo -n "choose gradle version [4 - 4.10, 5 - 5.6.2]:"
     read choose
-    if [ -z $choose ] || [ $choose="1" ]; then
+    if [ -z $choose ] || [ $choose = 4 ]; then
         gradle_version=4
-    elif [ $choose="5" ]; then
+    elif [ $choose = 5 ]; then
         gradle_version=5
     fi
 fi
